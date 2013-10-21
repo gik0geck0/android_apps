dd605b1c53405417758c002eac099d86d9365220
Matt Buland <gik0geck0@gmail.com>
2013-10-21 15:51:47 -0600

    Haha! Get the project building/running so long as android-rss is in .. and its jar is in libs/

--- weather_app/src/edu/mines/weatherapp/MainActivity.java
+++ weather_app/src/edu/mines/weatherapp/MainActivity.java
@@ -3,6 +3,7 @@ package edu.mines.weatherapp;
 import android.app.Activity;
 import android.os.Bundle;
 import org.mcsoxford.rss.RSSFeed;
+import edu.mines.weatherapp.WeatherParser;
 
 public class MainActivity extends Activity
 {
--- weather_app/src/edu/mines/weatherapp/WeatherParser.java
+++ weather_app/src/edu/mines/weatherapp/WeatherParser.java
@@ -3,12 +3,19 @@ package edu.mines.weatherapp;
 //import java.lang.String;
 import org.mcsoxford.rss.RSSFeed;
 import org.mcsoxford.rss.RSSReader;
+import java.lang.Exception;
 
 class WeatherParser {
 
-    public static RSSFeed getRSSFeed(String url) {
+    public static RSSFeed getRSSFeed() {
         RSSReader reader = new RSSReader();
         String uri = "http://weather.yahooapis.com/forecastrss?w=2411762";
-        return reader.load(uri);
+        try {
+            RSSFeed retfeed = reader.load(uri);
+            return retfeed;
+        } catch (Exception e) {
+            System.out.println(e.getStackTrace());
+            return null;
+        }
     }
 }
